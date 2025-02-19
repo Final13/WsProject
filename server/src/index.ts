1f1481f076f8b20c965811f85a5dcb4b87304ec3
Ivan Matskevich <faxtalk666@gmail.com>
2025-02-20 01:01:40 +0300

    Add mongodb

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,26 @@
 import express from "express";
 import http from "http";
+import { MongoClient, ObjectId } from "mongodb";
 
 const app = express();
 const server = http.createServer(app);
 
+const uri = "mongodb://localhost:27017";
+const client = new MongoClient(uri);
+
 async function run() {
   try {
+    await client.connect();
+    const db = client.db("wsapp");
+    const collection = db.collection("messages");
+
     const PORT = 3001;
     server.listen(PORT, () => {
       console.log(`Server is running on http://localhost:${PORT}`);
     });
 
     app.get("/messages", async (req, res) => {
-      const messages = ["msg1", "msg2", "msg3"];
+      const messages = await collection.find().toArray();
       res.json(messages);
     });
   } catch (err) {
